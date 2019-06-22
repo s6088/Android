57e4ab8dfca40fa86e59f6a323dbafa92b536aaa
s6088 <sadmanahmed0@gmail.com>
2019-06-22 16:18:04 +0600

    save

--- DriverJnUBus/app/src/main/java/com/example/driverjnubus/HomeActivity.java
+++ DriverJnUBus/app/src/main/java/com/example/driverjnubus/HomeActivity.java
@@ -11,7 +11,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     private static final int PERMISSIONS_REQUEST = 1;
 
